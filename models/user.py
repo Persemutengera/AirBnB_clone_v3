471887173f95211a70c8c4045923e30ec4bdffed
Christian <111076892+i-christian@users.noreply.github.com>
2024-01-09 00:04:05 +0200

    update user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Holds class User """
 import models
+import hashlib
 from models.base_model import BaseModel, Base
 import os
 import sqlalchemy
@@ -26,6 +27,22 @@ class User(BaseModel, Base):
         first_name = ""
         last_name = ""
 
+
     def __init__(self, *args, **kwargs):
-        """Initializes user"""
+        """
+            instantiates user object
+        """
+        if kwargs:
+            pwd = kwargs.pop('password', None)
+            if pwd:
+                User.__set_password(self, pwd)
         super().__init__(*args, **kwargs)
+
+    def __set_password(self, pwd):
+        """
+            custom setter: encrypts password to MD5
+        """
+        secure = hashlib.md5()
+        secure.update(pwd.encode("utf-8"))
+        secure_password = secure.hexdigest()
+        setattr(self, "password", secure_password)
